62901c867fcab89391fb1a93a96e45dbb864417b
Vincent COURTOIS <vincent.courtois@ecov.fr>
2025-09-17 16:35:41 +0200

    Add some exports

---
+++ src/core/api/index.ts
@@ -0,0 +1,3 @@
+export type { ApiCable } from "./ApiCable.type"
+export type { ApiModule } from "./ApiModule.type"
+export type { ApiSynthesizer } from "./ApiSynthesizer.type"
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export type {
   Synthesizer, Module, Cable, ModuleLink, ModuleNode, NodeGenerator, Port, MonophonicNode, PolyphonicNode
 } from "./core/business/index.js"
+
+export type {
+  ApiModule, ApiCable, ApiSynthesizer
+} from "./core/api/index.js"
+
+export { SynthesizerBuilder } from "./builders/SynthesizerBuilder.js"
