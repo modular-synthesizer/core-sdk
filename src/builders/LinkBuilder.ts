3ae7b95602046b919d6a7007a1a0d5efdcf01684
Vincent COURTOIS <vincent.courtois@ecov.fr>
2025-10-24 15:42:10 +0200

    Fix an issue where ApiModuleNode were instanciated instead of ModuleNode

--- src/builders/LinkBuilder.ts
+++ src/builders/LinkBuilder.ts
@@ -1,15 +1,16 @@
-import type { ApiModule, ApiModuleLink } from "../core/api/ApiModule.type.js";
+import type { Module } from "../core/business/Module.type"
+import type { ApiModuleLink } from "../core/api/ApiModule.type.js";
 import type { BaseLink, ModuleLink } from "../core/business/ModuleLink.type.js";
 import type { ModuleNode } from "../core/business/ModuleNode.type.js";
 
 const PARAM_REGEX = /^[^.]+\.{1}[^.]+$/
 
-function findNode({ nodes }: ApiModule, name: string): ModuleNode {
+function findNode({ nodes }: Module, name: string): ModuleNode {
   const parsedName = name.match(PARAM_REGEX) ? name.split(".")[0] : name
-  return nodes.find(n => n.name === parsedName) as ModuleNode
+  return nodes[parsedName]
 }
 
-export function LinkBuilder(link: ApiModuleLink, module: ApiModule): ModuleLink {
+export function LinkBuilder(link: ApiModuleLink, module: Module): ModuleLink {
   const result: BaseLink = {
     id: link.id,
     from: { node: findNode(module, link.from.node), index: link.from.index },
--- src/builders/ModuleBuilder.ts
+++ src/builders/ModuleBuilder.ts
@@ -8,14 +8,17 @@ import { PortBuilder } from "./PortBuilder.js";
 
 export function ModuleBuilder(module: ApiModule): Module {
   const nodes = module.nodes.map(NodeBuilder)
-  const links = module.links.map(l => LinkBuilder(l, module))
   const result: Module = {
     id: module.id,
     nodes: indexOn(nodes, "name"),
-    links: indexOn(links, "id"),
+    links: {},
     ports: {},
     parameters: {}
   }
+
+  const links = module.links.map(l => LinkBuilder(l, result))
+  result.links = indexOn(links, "id")
+
   const ports = module.ports.map(p => PortBuilder(p, result))
   result.ports = indexOn(ports, "name")
 
