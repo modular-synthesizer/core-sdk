6415ef61e3c61755f24418c656618aa377eba702
Vincent COURTOIS <vincent.courtois@ecov.fr>
2025-11-05 15:21:59 +0100

    Add tests for medium and large knobs

--- spec/builders/ControlBuilder.spec.ts
+++ spec/builders/ControlBuilder.spec.ts
@@ -10,17 +10,23 @@ describe("ControlBuilder", () => {
     id: "module-id",
     rack: 0, slot: 0, slots: 2,
     nodes: [
-      { id: "target-1", name: "target-one", generator: "createGain", polyphonic: false }
+      { id: "target-1", name: "target-one", generator: "createGain", polyphonic: false },
+      { id: "target-2", name: "target-two", generator: "createGain", polyphonic: false },
+      { id: "target-3", name: "target-three", generator: "createGain", polyphonic: false },
     ],
     links: [],
     ports: [],
     parameters: [],
     type: "",
     controls: [
-      { id: "small-knob-id", component: "SmallKnob", payload: { x: 10, y: 20, target: "target-one", label: "test" }}
+      { id: "small-knob-id", component: "SmallKnob", payload: { x: 10, y: 20, target: "target-one", label: "test" } },
+      { id: 'knob-id', component: 'Knob', payload: { x: 11, y: 21, target: 'target-two', label: "test2" } },
+      { id: "large-knob-id", component: "LargeKnob", payload: { x: 12, y: 22, target: "target-three", label: "test3" } },
     ]
   }
+
   const instanciated: Module = ModuleBuilder(module)
+
   describe("SmallKnob", () => {
     const smallKnob: Control = ControlBuilder(module.controls[0], instanciated) as Control
 
@@ -41,6 +47,52 @@ describe("ControlBuilder", () => {
     })
     it("Targets the correct node", () => {
       expect(smallKnob.payload.target?.id).toEqual("target-1")
+    })
+  })
+
+  describe("Knob", () => {
+    const knob: Control = ControlBuilder(module.controls[1], instanciated) as Control
+
+    it("Has the correct ID", () => {
+      expect(knob.id).toEqual("knob-id")
+    })
+    it("has the correct comonent", () => {
+      expect(knob.component).toEqual("Knob")
+    })
+    it("Has the correct X coordinate", () => {
+      expect(knob.payload.x).toBe(11)
+    })
+    it("Has the correct Y coordinate", () => {
+      expect(knob.payload.y).toBe(21)
+    })
+    it("Has the correct label", () => {
+      expect(knob.payload.label).toBe("test2")
+    })
+    it("Targets the correct node", () => {
+      expect(knob.payload.target?.id).toEqual("target-2")
+    })
+  })
+
+  describe("LargeKnob", () => {
+    const largeKnob: Control = ControlBuilder(module.controls[2], instanciated) as Control
+
+    it("Has the correct ID", () => {
+      expect(largeKnob.id).toEqual("large-knob-id")
+    })
+    it("has the correct comonent", () => {
+      expect(largeKnob.component).toEqual("LargeKnob")
+    })
+    it("Has the correct X coordinate", () => {
+      expect(largeKnob.payload.x).toBe(12)
+    })
+    it("Has the correct Y coordinate", () => {
+      expect(largeKnob.payload.y).toBe(22)
+    })
+    it("Has the correct label", () => {
+      expect(largeKnob.payload.label).toBe("test3")
+    })
+    it("Targets the correct node", () => {
+      expect(largeKnob.payload.target?.id).toEqual("target-3")
     })
   })
 })
--- src/core/business/Module.type.ts
+++ src/core/business/Module.type.ts
@@ -1,4 +1,4 @@
-import { Control } from "./Control.type.js"
+import type { Control } from "./Control.type.js"
 import type { ModuleLink } from "./ModuleLink.type.js"
 import type { ModuleNode } from "./ModuleNode.type.js"
 import type { Parameter } from "./Parameter.type.js"
@@ -12,15 +12,16 @@ export type ModuleBox = {
 }
 
 export type Module = ModuleBox & {
+  // The unique identifier of the module.
   id: string,
-  // Inner nodes are indexed on their names
+  // Inner nodes are indexed on their names.
   nodes: Record<string, ModuleNode>,
-  // Inner links are indexed on their UUID
+  // Inner links are indexed on their UUID.
   links: Record<string, ModuleLink>
-
+  // The different ports where cables can be plugged in.
   ports: Record<string, Port>
-
+  // The different parameters setting AudioParam values.
   parameters: Record<string, Parameter>
-
+  // The different controls setting parameters values.
   controls: Control[]
 }
