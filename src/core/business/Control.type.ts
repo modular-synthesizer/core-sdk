cc186aa97396975ec7d1412e1c782e01cda0d1f0
Vincent <courtois.vincent@outlook.com>
2025-11-07 21:36:30 +0100

    Add id to controls

--- src/core/business/Control.type.ts
+++ src/core/business/Control.type.ts
@@ -1,8 +1,9 @@
 import type { GenericKnob } from "../api/ApiControl.type";
+import { Identified } from "../utils/Identified.type";
 import type { Parameter } from "./Parameter.type";
 
 export type SmallKnob = GenericKnob<"SmallKnob", Parameter>
 export type Knob = GenericKnob<"Knob", Parameter>
 export type LargeKnob = GenericKnob<"LargeKnob", Parameter>
 
-export type Control = SmallKnob | Knob | LargeKnob
+export type Control = Identified & (SmallKnob | Knob | LargeKnob)
